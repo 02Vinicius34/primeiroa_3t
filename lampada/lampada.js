1e47692254ad09f503f12e7c607866d7fc58e5a8
02Vinicius34 <110027528+02Vinicius34@users.noreply.github.com>
2023-06-21 22:20:48 -0300

    098

---
+++ lampada/lampada.js
@@ -0,0 +1,12 @@
+const turnOn = document.getElementById('ligada');
+const lamp = document.getElementById('lamp');
+
+
+function ligar (){
+    lamp.
+    rc ='img/ligada.jpeg'
+}
+
+
+
+turnOn.addEventListener('click', ligar)
